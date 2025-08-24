bb4c99d8f3c3f847105c30ed5303c025b8fb15b6
AKhoa2109 <anhkhoa2109.it@gmail.com>
2025-08-24 14:22:39 +0700

    Commit feature create-user /api/users/create and postman.json

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,23 @@
 import dotenv from 'dotenv';
 import express from 'express';
-import connectDB from './config/configDB';
-
+import connectDB from './config/db/configDB';
+import apiRouter from './routes';
+import morgan from 'morgan';
 dotenv.config();
+
+
 
 const port = process.env.PORT || 4000;
 
 const app = express();
 
+
 connectDB();
 
+app.use(express.json());
+app.use(morgan('combined'));
 
-app.use(express.json);
+app.use('/api',apiRouter);
 
 
 app.listen(port, () => {
