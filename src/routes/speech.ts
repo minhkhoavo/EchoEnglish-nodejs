c50a92d8dfabcf1858f190780f4d0dcc7c7b9311
vovantri123 <vovantri204@gmail.com>
2025-09-22 12:20:03 +0700

    refactor: remove unnecessary 'next' parameter from SpeechController methods

--- src/routes/speech.ts
+++ src/routes/speech.ts
@@ -5,18 +5,18 @@ import { uploadSingle } from '~/config/multerConfig.js';
 const router = Router();
 
 router.post('/assess', uploadSingle, (req, res, next) =>
-    SpeechController.assess(req, res, next)
+    SpeechController.assess(req, res)
 );
 
 // Recording
 router.get('/recordings/', (req, res, next) =>
-    SpeechController.listRecordings(req, res, next)
+    SpeechController.listRecordings(req, res)
 );
 router.get('/recordings/:id', (req, res, next) =>
-    SpeechController.detailRecording(req, res, next)
+    SpeechController.detailRecording(req, res)
 );
 router.delete('/recordings/:id', (req, res, next) =>
-    SpeechController.removeRecording(req, res, next)
+    SpeechController.removeRecording(req, res)
 );
 
 export default router;
